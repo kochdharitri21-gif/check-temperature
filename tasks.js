809b64a0be3dd5da214ad95d19dc4cde8c6f7582
dharitri <kochdharitri21@gmail.com>
2026-09-23 00:31:18 +0530

    function

---
+++ tasks.js
@@ -0,0 +1,21 @@
+function getMenuChoice(choice) {
+  let result;
+  switch (choice) {
+    case 1:
+      result = "Start Game";
+      break;
+    case 2:
+      result = "Load Game";
+      break;
+    case 3:
+      result = "Settings";
+      break;
+    case 4:
+      result = "Exit";
+      break;
+    default:
+      result = "Invalid Choice";
+      break;
+  }
+  return result;
+}
